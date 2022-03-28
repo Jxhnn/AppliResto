60d76101adec43ae24f35865a9d9938d98e6154e
john.thomas <john.thomas@inbox.lv>
2022-03-28 09:25:52 +0200

    login start + Manifest update

--- app/src/main/java/btssio/appliresto/controleur/MainActivity.java
+++ app/src/main/java/btssio/appliresto/controleur/MainActivity.java
@@ -1,15 +1,37 @@
 package btssio.appliresto.controleur;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import btssio.appliresto.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText passEdit, mailEdit;
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        passEdit = findViewById(R.id.editTextId);
+        mailEdit = findViewById(R.id.editTextMdp);
+        loginButton = findViewById(R.id.buttonConnect);
+
+        loginButton.setOnClickListener(loginEvent);
     }
+
+    private View.OnClickListener loginEvent = new View.OnClickListener() {
+        public void onClick(View v) {
+            Log.d("DEBUG: ", "Login button clicked");
+        }
+    };
 }
