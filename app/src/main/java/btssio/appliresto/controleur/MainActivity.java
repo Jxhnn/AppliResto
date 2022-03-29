7f47b2b53aa12d1e73e8f5bbcfca98c5d68c48bd
John Ward <36800890+Jxhnn@users.noreply.github.com>
2022-03-29 02:41:38 +0200

    DESIGN LOGIN #1

--- app/src/main/java/btssio/appliresto/controleur/MainActivity.java
+++ app/src/main/java/btssio/appliresto/controleur/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import btssio.appliresto.R;
@@ -19,14 +21,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private EditText passEdit, mailEdit;
     private Button loginButton;
+    private TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+
+
 
         mailEdit = findViewById(R.id.editTextId);
-        passEdit = findViewById(R.id.editTextMdp);
+        passEdit = findViewById(R.id.editTextPass);
+
 
         loginButton = findViewById(R.id.buttonConnect);
 
