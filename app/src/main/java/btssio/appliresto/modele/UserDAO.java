c3af008b111e9eebfd5c1d12f2739d8d2ddcb6c7
ben.guillet <ben.leguillet@gmail.com>
2022-03-31 14:45:52 +0200

    proo

--- app/src/main/java/btssio/appliresto/modele/UserDAO.java
+++ app/src/main/java/btssio/appliresto/modele/UserDAO.java
@@ -1,8 +1,33 @@
 package btssio.appliresto.modele;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by mick.souloumiac1 on 17/03/2022.
  */
 
 public class UserDAO {
+
+    private static String base = "BDresto";
+    private static int version = 1;
+    BD_SQLiteOpenHelper accesBD;
+
+    public UserDAO(Context ct){
+        accesBD = new BD_SQLiteOpenHelper(ct, base, null, version);
+    }
+
+
+    public long updateUser(User newUser, String mailU){
+        long ret;
+        SQLiteDatabase bd = accesBD.getWritableDatabase();
+
+        ContentValues value = new ContentValues();
+        value.put("pseudoU", newUser.getPseudoU());
+        value.put("mdpU", newUser.getMdpU());
+        ret = bd.update("User", null, "mailU" + " = " + mailU, null);
+
+        return ret;
+    }
 }
